0a8c5ccb5d9a70ce38ec53a280f9fbf785a75c81
cevdetkilickeser <kilickesercevdet@gmail.com>
2024-01-03 14:42:11 +0300

    Update project

--- app/src/main/java/com/cevdetkilickeser/holdthenote/ui/activity/MainActivity.kt
+++ app/src/main/java/com/cevdetkilickeser/holdthenote/ui/activity/MainActivity.kt
@@ -2,12 +2,13 @@ package com.cevdetkilickeser.holdthenote.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.cevdetkilickeser.holdthenote.R
+import com.cevdetkilickeser.holdthenote.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
